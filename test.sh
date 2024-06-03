37887ed80521ff691f82899e95558a8f2b76c835
Mounikareddy16 <45049658+Mounikareddy16@users.noreply.github.com>
2024-06-03 22:19:26 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ get_new_files() {
   # Get the list of new files added in the current branch compared to the default branch
   #new_files=$(git diff --name-only --diff-filter=A origin/$default_branch...$current_branch)
   git checkout master 
-  new_files=$(git diff testing)
+  new_files=$(git diff --name-only --diff-filter=M master testing)
 
   echo "$new_files"
 }
